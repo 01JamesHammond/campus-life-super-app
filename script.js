a2322b4264a582591076e0e37e2f2a88cdfb9393
01jameshammond <01jameshammond@gmail.com>
2025-10-19 22:12:04 -0500

    Final project submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+    
+    //The dictionary to keep track of locations for the interactive map
     const myCoordnates = {
         Schewe: "39.73132677118451,-90.24614954675675"
     };
 
-
-    let currentCoordinates;
+    // Declaring variables
+    let currentCoordinates; // This will hold the coordinates depending on which item is chosen.
     const dropDown = document.getElementById("options");
     const originalContainer = document.querySelector('.maps_flag_marker'); // This is a copy of the feedback div, the original
 
